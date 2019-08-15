e397a31780500fab79a8cbdd923edaf341f55f05
Steve Womack <swomack@pivotal.io>
2019-08-15 15:28:22 -0500

    Adds config test

--- src/main/java/com/at/configtest/ConfigTestApplication.java
+++ src/main/java/com/at/configtest/ConfigTestApplication.java
@@ -1,7 +1,11 @@
 package com.at.configtest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ConfigTestApplication {
@@ -10,4 +14,25 @@ public class ConfigTestApplication {
 		SpringApplication.run(ConfigTestApplication.class, args);
 	}
 
+	@RefreshScope
+	@RestController
+	class MessageRestController {
+
+		@Value("${message}")
+		private String message;
+
+		@RequestMapping("/message")
+		String getMessage() {
+			System.out.println("MESSAGE CALLED");
+			return this.message;
+		}
+
+		@RequestMapping("/wait")
+		String timeout() throws InterruptedException {
+			System.out.println("TIMEOUT CALLED");
+			Thread.sleep(30000);
+			return this.message;
+		}
+
+	}
 }
